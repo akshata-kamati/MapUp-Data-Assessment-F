7efb25a31261612e7c3ef7f83d6b61e3e86e9450
akshata-kamati <60888142+akshata-kamati@users.noreply.github.com>
2023-12-10 20:00:00 +0530

    python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -14,7 +14,20 @@ def generate_car_matrix(df)->pd.DataFrame:
     """
     # Write your logic here
 
-    return df
+    --return df
+    import pandas as pd
+
+def generate_car_matrix(dataset):
+    
+    car_matrix = dataset.pivot(index='id_1', columns='id_2', values='car').fillna(0)
+
+   
+    car_matrix.values[[range(len(car_matrix))]*2] = 0
+
+    return car_matrix
+
+
+
 
 
 def get_type_count(df)->dict:
@@ -28,8 +41,25 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
+import pandas as pd
 
-    return dict()
+def get_type_count(dataset):
+    
+    dataset['car_type'] = pd.cut(dataset['car'],
+                                 bins=[-float('inf'), 15, 25, float('inf')],
+                                 labels=['low', 'medium', 'high'],
+                                 include_lowest=True, right=False)
+
+   
+    type_counts = dataset['car_type'].value_counts().to_dict()
+
+    sorted_type_counts = {key: type_counts[key] for key in sorted(type_counts)}
+
+    return sorted_type_counts
+
+
+
+   
 
 
 def get_bus_indexes(df)->list:
@@ -44,7 +74,22 @@ def get_bus_indexes(df)->list:
     """
     # Write your logic here
 
-    return list()
+   import pandas as pd
+
+def get_bus_indexes(dataset):
+  
+    bus_mean = dataset['bus'].mean()
+
+    
+    bus_indexes = dataset[dataset['bus'] > 2 * bus_mean].index.tolist()
+
+
+    bus_indexes.sort()
+
+    return bus_indexes
+
+
+
 
 
 def filter_routes(df)->list:
@@ -58,8 +103,23 @@ def filter_routes(df)->list:
         list: List of route names with average 'truck' values greater than 7.
     """
     # Write your logic here
+import pandas as pd
 
-    return list()
+def filter_routes(dataset):
+   
+    route_avg_truck = dataset.groupby('route')['truck'].mean()
+
+  
+    selected_routes = route_avg_truck[route_avg_truck > 7].index.tolist()
+
+    
+    selected_routes.sort()
+
+    return selected_routes
+
+
+
+    
 
 
 def multiply_matrix(matrix)->pd.DataFrame:
@@ -73,8 +133,22 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
+import pandas as pd
 
-    return matrix
+def multiply_matrix(result_matrix):
+    
+    modified_matrix = result_matrix.copy()
+
+    modified_matrix[modified_matrix > 20] *= 0.75
+    modified_matrix[modified_matrix <= 20] *= 1.25
+
+    
+    modified_matrix = modified_matrix.round(1)
+
+    return modified_matrix
+
+
+
 
 
 def time_check(df)->pd.Series:
@@ -89,4 +163,23 @@ def time_check(df)->pd.Series:
     """
     # Write your logic here
 
-    return pd.Series()
+    import pandas as pd
+
+def check_timestamp_completeness(dataset):
+    
+    dataset['start_timestamp'] = pd.to_datetime(dataset['startDay'] + ' ' + dataset['startTime'])
+    dataset['end_timestamp'] = pd.to_datetime(dataset['endDay'] + ' ' + dataset['endTime'])
+
+  
+    completeness_check = (
+        (dataset['end_timestamp'] - dataset['start_timestamp'] == pd.Timedelta(days=1) - pd.Timedelta(seconds=1)) &
+        (dataset['start_timestamp'].dt.dayofweek == 0) &
+        (dataset['end_timestamp'].dt.dayofweek == 6)
+    )
+
+    
+    completeness_series = completeness_check.groupby(['id', 'id_2']).all()
+
+    return completeness_series
+
+
